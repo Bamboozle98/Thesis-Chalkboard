4a9915cf9bb641b8e71e72851928e0e2455c71d4
Bamboozle98 <mccutcheonc18@students.ecu.edu>
2024-11-01 11:58:16 -0400

    Updated Reqs

--- model/TheMask.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def mask_superpixel(image, segments, superpixel_id):
-    # Create mask for the given superpixel
-    mask = np.zeros_like(image)
-    mask[segments == superpixel_id] = image[segments == superpixel_id]
-    return mask
