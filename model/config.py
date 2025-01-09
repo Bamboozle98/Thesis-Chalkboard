eda52d4ce10ddfcf14e770120528aa8686ae4814
Bamboozle98 <cbrannonm98@gmail.com>
2025-01-09 18:19:55 -0500

    Skeleton files

--- model/config.py
+++ model/config.py
@@ -1,4 +1,4 @@
 num_epochs = 20
 learning_rate = 1e-4
-dataset_dir = 'C:/Users/cbran/PycharmProjects/Thesis-Chalkboard/Data/images'
+dataset_dir = 'C:/Users/cmccu/PycharmProjects/Thesis-Chalkboard/data/images'
 batch_size = 4
